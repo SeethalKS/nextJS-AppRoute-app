f5d4e06eaf26b634713929180fea0e018d45aa80
SeethalKS <seethal007@outlook.com>
2025-01-29 16:49:27 +0530

    footer code correction

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,17 +1,30 @@
-"use client"; 
+// "use client"; 
 
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+
+// export default function Footer() {
+//   const [clientLoaded, setClientLoaded] = useState(false);
+
+//   useEffect(() => {
+//     setClientLoaded(true);
+//   }, []);
+
+//   return (
+//     <footer>
+//       {clientLoaded && <p>{document.title}</p>} {/* Example usage */}
+//     </footer>
+//   );
+// }
+'use client'
+import React, { useEffect } from 'react'
 
 export default function Footer() {
-  const [clientLoaded, setClientLoaded] = useState(false);
-
-  useEffect(() => {
-    setClientLoaded(true);
-  }, []);
-
+    useEffect(()=>{
+        console.log("Fetch call from useeffect-Footer");
+        fetch('https://fakestoreapi.com/products')
+    },[]);
+    console.log("Footer");
   return (
-    <footer>
-      {clientLoaded && <p>{document.title}</p>} {/* Example usage */}
-    </footer>
-  );
+    <div>Footer</div>
+  )
 }
